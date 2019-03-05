98eb8b43145c46ee2faf403f99019be7da75d3a1
Russell Carter <russell.c12@gmail.com>
2019-03-05 10:15:55 -0500

    styled form

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,16 +1,32 @@
 import React from 'react'
 
 const Weather = props => (
-  <div classNAme="weather__info">
+  <div className="weather__info">
     {/* if this states exist, display the text */}
     {props.city && props.country && (
-      <p>
-        Location: {props.city}, {props.country}
+      <p className="weather__key">
+        Location:{' '}
+        <span className="weather__value">
+          {' '}
+          {props.city}, {props.country}
+        </span>
       </p>
     )}
-    {props.temperature && <p>Temperature: {props.temperature}</p>}
-    {props.humidity && <p>Humidity: {props.humidity}</p>}
-    {props.description && <p>Conditions: {props.description}</p>}
+    {props.temperature && (
+      <p className="weather__key">
+        Temperature: <span className="weather__value">{props.temperature}</span>
+      </p>
+    )}
+    {props.humidity && (
+      <p className="weather__key">
+        Humidity: <span className="weather__value">{props.humidity}</span>
+      </p>
+    )}
+    {props.description && (
+      <p className="weather__key">
+        Conditions: <span className="weather__value">{props.description}</span>
+      </p>
+    )}
     {props.error && <p>{props.error}</p>}
   </div>
 )
